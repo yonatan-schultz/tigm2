d39161e69fb164dd68ebf2350fb98287ed37fb30
Yonatan Schultz <yonatan.schultz@gmail.com>
2015-05-08 09:33:50 -0700

    Users now have first, last name and username

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations"}
   resources :games
   resources :played_games
   scope "games/:game_id" do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424190755) do
+ActiveRecord::Schema.define(version: 20150508160946) do
 
   create_table "games", force: :cascade do |t|
     t.boolean  "shattered_empire"
@@ -50,6 +50,9 @@ ActiveRecord::Schema.define(version: 20150424190755) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
